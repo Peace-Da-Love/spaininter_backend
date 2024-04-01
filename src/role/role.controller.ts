58c1de59ab412cb4426f7ea801b401a6dc31b810
Stepan <stepan663663@gmail.com>
2024-04-01 23:12:39 +0300

    refactor: role controller

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { AdminAuth } from '../auth/decorators/admin.decorator';
 
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @AdminAuth()
   @Post()
   public async createRole(@Body() dto: CreateRoleDto) {
     return this.roleService.createRole(dto);
