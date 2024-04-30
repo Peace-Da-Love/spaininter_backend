0ecf017a00a7638b90c6e9c549aec1edc43ef5b2
Степан Никитин <stepannikitin@MacBook-Air-Stepan.local>
2024-05-01 01:08:20 +0300

    fixed: link formatter bug

--- src/common/utils/remove-invalid-characters.ts
+++ src/common/utils/remove-invalid-characters.ts
@@ -1,6 +1,6 @@
 export function removeInvalidCharacters(str: string) {
   // Remove all characters that are not letters in any language
-  const regExp = /[^\p{L}\p{M}\s]/gu;
+  const regExp = /[^\p{L}\p{N}\p{M}\s]/gu;
   return str
     .toLowerCase()
     .replace(regExp, '')
