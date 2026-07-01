bfd26510c26e37f0a4735b7e5da22250913f2724
Валерий <valera210597@gmail.com>
2026-07-01 17:52:52 +0300

    fix(RegionalTelegramTarget): update forum groups messageThreadIds

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -58,12 +58,21 @@ export class NewsService {
     chatId: '-1002067491776',
   };
   private readonly regionalTelegramTargets: Record<
-    string,
-    RegionalTelegramTarget
+  string,
+  RegionalTelegramTarget
   > = {
-    torrevieja: { chatId: '-1001303923147' },
-    valencia: { chatId: '-1002106340245' },
-    barcelona: { chatId: '-1002051856007' },
+    torrevieja: {
+      chatId: '-1001303923147',
+      messageThreadId: 9176,
+    },
+    valencia: {
+      chatId: '-1002106340245',
+      messageThreadId: 8,
+    },
+    barcelona: {
+      chatId: '-1002051856007',
+      messageThreadId: 61,
+    },
   };
 
   constructor(
